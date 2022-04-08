856c04fafd15af070a9cff8a9f7c3c5a2aa74c97
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-04-08 21:05:07 +0800

    upd

--- src/Network0.js
+++ src/Network0.js
@@ -2,6 +2,8 @@ var StationList = [];
 var TotalStation  = 50;
 var LineColorList= "2d00f7-6a00f4-8900f2-a100f2-b100e8-bc00dd-d100d1-db00b6-e500a4-f20089".split("-").map(a=>"#"+a);
 // var LineColorList = ['red' , 'green' , 'blue']
+var LineList = []
+
 
 function setup() {
     frameRate(24);
@@ -17,6 +19,63 @@ function setup() {
     for( let i=0;i<TotalStation ; i++){
         StationList.push( CreateStation( i ) );
     }
+
+    // Create Line
+    for(let i=0;i<TotalStation;i++){
+        let cnt = random(0,5);
+
+        for(let j=0;j<cnt ;j++){
+                let ith = int(random(0,TotalStation-1) );
+
+                let clr = color(random(LineColorList))
+                // clr.setAlpha(150 + noise(frameCount/10,frameCount)*10)
+                // fill( clr );
+
+                LineList.push({
+                    A:i,
+                    B:ith
+                })
+                // line(StationList[i].x , StationList[i].y, StationList[ ith ].x , StationList[ith].y );
+        }
+    }
+    
+}
+
+function draw(){
+    // Draw Station 
+    blendMode(BLEND);
+    background(0);
+    StationList.forEach(ele => {
+        fill(ele.clr);
+        ellipse(ele.x+noise(frameCount/10 ,ele.x/5)*10 , ele.y+noise(frameCount/10 ,ele.y/5)*10 ,ele.sz , ele.sz);
+    });
+
+    blendMode(SCREEN);
+    // Draw Lines
+    for(let i=0;i<TotalStation;i++){
+        let cnt = random(0,5);
+
+        for(let j=0;j<cnt ;j++){
+                let ith = int(random(0,TotalStation-1) );
+
+                let clr = color(random(LineColorList))
+                // clr.setAlpha(150 + noise(frameCount/10,frameCount)*10)
+                fill( clr );
+                line(StationList[i].x , StationList[i].y, StationList[ ith ].x , StationList[ith].y );
+        }
+    }
+}
+
+function CreateStation( ith ){
+    return{
+        x : width/2+random(200,300)*cos(ith),
+        y : height/2+random(200,300)*sin(ith),
+        clr : color( random(200,255)),
+        sz : random(5,20)
+    };
+}
+
+function DrawStation(){
     // Draw Station 
     blendMode(BLEND);
     StationList.forEach(ele => {
@@ -38,35 +97,6 @@ function setup() {
                 line(StationList[i].x , StationList[i].y, StationList[ ith ].x , StationList[ith].y );
         }
     }
-
-    noLoop();
-}
-
-function draw(){
-    StationList.forEach(ele => {
-        fill(ele.clr);
-        ellipse(ele.x , ele.y ,ele.sz , ele.sz);
-    });
-  
-    for(let i=0;i<TotalStation;i++){
-      let cnt = random(0,10);
-      
-      for(let j=0;j<cnt ;j++){
-        let ith = int(random(0,TotalStation-1) );
-            stroke( random(LineColorList));
-            strokeWeight( random( 0.5,1) );
-            line(StationList[i].x , StationList[i].y, StationList[ ith ].x , StationList[ith].y );
-      }
-    }
-}
-
-function CreateStation( ith ){
-    return{
-        x : width/2+random(100,300)*cos(ith),
-        y : height/2+random(100,300)*sin(ith),
-        clr : color( random(200,255)),
-        sz : random(5,20)
-    };
 }
 
 
