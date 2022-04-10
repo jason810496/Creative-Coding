0775a3bce4a4cf1f0502d7678648407e2b4fcf2b
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-04-10 22:16:51 +0800

    upd

--- src/sin0.js
+++ src/sin0.js
@@ -1,71 +1,69 @@
+var K = 250;
+var Scale = 4;
+var Count = 230;
+var flag = false;
+var Total = 20;
+var Total_mid_circlr =5;
+var Span = 6;
+
 function setup() {
-    createCanvas(400, 400);
-    angleMode(DEGREE);
-    shearX(60);
+  createCanvas(800, 800);
+  angleMode(DEGREES);
+  colorMode(HSB);
+  shearX(60);
+  translate(width/2,height/2);
+}
+  
+function draw() {
+  
+  
+  blendMode(BLEND);
+  translate(width/2,height/2);
+  // rotate(frameCount);
+  // shearX(-30);
+  scale( 2 , 1);
+  background(0);
+  noFill();
+  
+  // middle line
+  let Line_clr = color('white');
+  Line_clr.setAlpha(random(150,255));
+  stroke( Line_clr );
+  strokeWeight( noise(frameCount/50 ,10)*7 );
+  line(0, -height/2, 0, height/2);
+  
+  // middle circle
+  for(let i=0;i<Total_mid_circle;i++){
+    circle()
   }
   
-  function draw() {
-    background(0);
-    noFill();
-    stroke(200);var K = 250;
-    var Scale = 4;
-    var Count = 230;
-    var flag = false;
-    var Total = 20;
-    var Span = 6;
-    
-    function setup() {
-      createCanvas(800, 800);
-      angleMode(DEGREES);
-      colorMode(HSB);
-      shearX(60);
-      translate(width/2,height/2);
-    }
-      
-    function draw() {
-      
-      
-      blendMode(BLEND);
-      translate(width/2,height/2);
-      // rotate(frameCount);
-      // shearX(-30);
-      scale( 2 , 1);
-      background(0);
-      noFill();
-      
-      strokeWeight(1);
-      
-      let R = 120;
-      
-      Calculate_Color()
-      for(let k=0; k< Total ;k++){
-          let i = k * Span;
-          blendMode(SCREEN);
-          stroke( (frameCount/5+i/2)%360 , 80 , 80);
-          circle( 0 ,sin(i/Scale+frameCount)*map(i,5,R,K,0)-2,i*3);
-          circle( 0 ,sin(i/Scale+frameCount+90)*map(i,5,R,K,0)+2,i*3);
-          circle( 0 ,sin(i/Scale+frameCount+180)*map(i,5,R,K,0)+2,i*3);
-          circle( 0 ,sin(i/Scale+frameCount+270)*map(i,5,R,K,0)+2,i*3);
-      }
-    }
-    
-    function Calculate_Color(){
-      if(flag ){
-        Count+=0.5;
-      }
-      else{
-        Count-=0.5;
-      }
-      if(Count>330 && flag){
-        flag=false;
-      }
-      if(Count<230 && !flag){
-        flag=true;
-      }
-    }
-    strokeWeight(3);
-
-    for(let i=0;i<120;i+=10){
-        circle(i,sin(frameCount)*10,i*5);
-    }
+  strokeWeight(1);
+  
+  let R = 120;
+  
+  Calculate_Color()
+  for(let k=0; k< Total ;k++){
+      let i = k * Span;
+      // blendMode(SCREEN);
+      stroke( (frameCount/5+i/2)%360 , 80 , 80);
+      circle( 0 ,sin(i/Scale+frameCount)*map(i,5,R,K,0)-2,i*3);
+      circle( 0 ,sin(i/Scale+frameCount+90)*map(i,5,R,K,0)+2,i*3);
+      circle( 0 ,sin(i/Scale+frameCount+180)*map(i,5,R,K,0)+2,i*3);
+      circle( 0 ,sin(i/Scale+frameCount+270)*map(i,5,R,K,0)+2,i*3);
   }
+}
+
+function Calculate_Color(){
+  if(flag ){
+    Count+=0.5;
+  }
+  else{
+    Count-=0.5;
+  }
+  if(Count>330 && flag){
+    flag=false;
+  }
+  if(Count<230 && !flag){
+    flag=true;
+  }
+}
