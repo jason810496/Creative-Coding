4d8e338ddc08fa26707d0e0f85abb2eca2d8bc28
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-05 10:51:30 +0800

    upd

--- src/Maze 0.js
+++ src/Maze 0.js
@@ -2,21 +2,52 @@ var OrigSize = 800;
 var pos = [ [0,1],  [1,0] , [0,-1] ,[-1,0] ,[1,1],[1,-1],[-1,1],[-1,-1] ];
 var span = 50;
 var ColorList = "f72585-b5179e-7209b7-560bad-480ca8-3a0ca3-3f37c9-4361ee-4895ef-4cc9f0".split("-").map(a=>"#"+a);
+var clr;
+var Node1;
+var Node2;
+
+
+
+class Node{
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+        this.depth=0;
+    }
+}
 
 function setup() {
     createCanvas(OrigSize, OrigSize);
-
     background(0);
-    push();
-        DFS(0,0,0);
-    pop();
+    ;
+    clr = color(0);
+    clr.setAlpha(50);
+
+    frameRate(10);
+
+    Node1 = { x:0,y:0,d:0};
+    let v= random( pos );
+    Node2 = { x:v[0],y:v[1],d:1};
 }
 
 function draw() {
-    // background(0);
+    if( Node2.depth >= 10 ){
+        Node1 = { x:0,y:0,d:0};
+        let v= random( pos );
+        Node2 = { x:v[0],y:v[1],d:1};
+    }
+    background(clr);
 
-    // let deg = noise(frameCount/100 ,10 )*(-50);
-    // Recursion(700,10 , deg ,1 ,0);
+    strokeWeight(5);
+    stroke(255);
+    line( Node1.x , Node1.x , Node2.x ,Node2.y );
+
+    Node1 = Node2;
+
+    Node2.depth++;
+    let v= random( pos );
+    Node2.x += v[0];
+    Node2.y += v[1];
 }
 
 function DFS(posX , posY ,depth){
