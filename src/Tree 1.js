985491e8ea1e750b8c23e7ac724607800673550e
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-09 12:12:53 +0800

    change to previous verison

--- src/Tree 1.js
+++ src/Tree 1.js
@@ -5,8 +5,6 @@ var Shift = 0;
 var RandDegList = [];
 var RandLenList = [];
 var RotateScale = 10;
-var BlossomList = [];
-var Count = 0;
 var ColorList = "f72585-b5179e-7209b7-560bad-480ca8-3a0ca3-3f37c9-4361ee-4895ef-4cc9f0".split("-").map(a=>"#"+a);
 
 function setup() {
@@ -29,26 +27,40 @@ function setup() {
 function draw() {
     background(0);
     translate(  width/2 , height-100 );
-
-    Count = 0;
     DrawTree();
 }
 
 function CreateTree( l ,w ,d , c1 , c2 ){
     
   
-    if( d>=5 ){
-
-        BlossomList.push( int( random(0,9) ) );
+    if( d>=6 ){
+      push()
+        let clr = color( ColorList[ int( map(noise(l*10,d*10,frameCount/30) ,0, 1 ,0,9) ) ] );
+        clr.setAlpha( noise(l,w)*50+50 );
+        noStroke();
+        fill(  clr );
+        circle( 0 , 0 , l);
+      pop()
     }
   
     if( d>6){
       return ;
     }
 
-    CreateTree( l*0.8 , w*0.6 ,d+1 ,c1*1.8, c2*1.2);
+    strokeWeight(w);
+    line( 0 ,0 ,0 ,-l );
+    push()
+        translate(0,-l);
+        push()
+            rotate( -Deg + sin(frameCount*c1)*c2 );
+            CreateTree( l*0.8 , w*0.6 ,d+1 ,c1*1.8, c2*1.2);
+        pop()
+        push()
+            rotate( Deg + + sin(frameCount*c1)*c2);
+            CreateTree( l*0.8 , w*0.6 ,d+1 ,c1*1. , c2*1.2);
+        pop()
 
-    CreateTree( l*0.8 , w*0.6 ,d+1 ,c1*1.8 , c2*1.2);
+    pop()
 }
 
 function CreateTree2( l , w  ){  // len , weidht , depth
@@ -70,7 +82,7 @@ function CreateTree2( l , w  ){  // len , weidht , depth
                 let ll =  l*0.8*random(1,1.5) ; 
                 let ww =  w*0.8*random(1,1.2) ;
                 RandLenList.push( { l:ll , w:ww } );
-                CreateTree( ll, ww );
+                // CreateTree( ll, ww );
             pop();
         }
 
@@ -91,44 +103,11 @@ function DrawTree(){
         RandDegList.forEach(deg => {
             push();
                 rotate( deg + sin( frameCount*1.5 )*RotateScale*1.1 );
-                DrawBranch( RandLenList[i].l , RandLenList[i].w ,1 , 1.8  , 1.5 ); // rotate scale1 , rotate scale2
+                CreateTree( RandLenList[i].l , RandLenList[i].w ,1 , 1.8  , 1.5 ); // rotate scale1 , rotate scale2
             pop();
 
             i++;
         });
 
     pop();
-}
-
-function DrawBranch( l ,w ,d , c1 , c2 ){
-    
-  
-    if( d>=5 ){
-      push()
-        let clr = color( ColorList[ BlossomList[Count++] ] );
-        clr.setAlpha( noise(l,w)*50+50 );
-        noStroke();
-        fill(  clr );
-        circle( 0 , 0 , l);
-      pop()
-    }
-  
-    if( d>6){
-      return ;
-    }
-
-    strokeWeight(w);
-    line( 0 ,0 ,0 ,-l );
-    push()
-        translate(0,-l);
-        push()
-            rotate( -Deg + sin(frameCount*c1)*c2 );
-            DrawBranch( l*0.8 , w*0.6 ,d+1 ,c1*1.8, c2*1.2);
-        pop()
-        push()
-            rotate( Deg + + sin(frameCount*c1)*c2);
-            DrawBranch( l*0.8 , w*0.6 ,d+1 ,c1*1.8 , c2*1.2);
-        pop()
-
-    pop()
 }
