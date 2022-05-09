698bf3dbdbc902b8d4844e74beb4cb8756e98a5a
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-09 10:09:48 +0800

    natural tree

--- src/Tree 0.js
+++ src/Tree 0.js
@@ -9,11 +9,11 @@ function setup() {
     background(0);
     stroke(255);
 
-    // translate(  width/2 , height-100 );
+    translate(  width/2 , height-100 );
 
     // CreateTree(Len , Wt );
 
-    
+    CreateTree2( Len*random(1,1.5) , Wt*random(1,1.2)  ,1 );
 }
   
 function draw() {
@@ -35,6 +35,27 @@ function CreateTree( l ,w){
             rotate( Deg);
             CreateTree( l*0.8 , w*0.6 );
         pop()
+
+    pop()
+}
+
+function CreateTree2( l , w ,d ){  // len , weidht , depth
+    if( l<10) return ;
+
+    strokeWeight(w);
+    line( 0 ,0 ,0 ,-l );
+
+    let cnt = int( random(2,2+d) );
+    let span = 2*Deg/cnt ;
+    push()
+        translate(0,-l);
+
+        for( let deg = -Deg ; deg <= Deg ; deg+=span ){
+            push();
+                rotate( deg + random(-30,30) );
+                CreateTree( l*0.8*random(1,1.5) , w*0.8*random(1,1.2) , d+1 );
+            pop();
+        }
 
     pop()
 }
