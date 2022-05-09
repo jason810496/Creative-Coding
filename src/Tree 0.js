ccb085a2baeb3d00de81dc92d1c210c03c72cd47
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-05-09 09:58:47 +0800

    TreeArray

--- src/Tree 0.js
+++ src/Tree 0.js
@@ -1,26 +1,27 @@
 var Deg = 30;
-var Len = 30;
+var Len = 40;
 var Wt = 4 ;
+var Shift = 0;
 function setup() {
     createCanvas(800, 800);
     angleMode(DEGREES);
 
+    background(0);
     stroke(255);
-    
-    translate(  width/2 , height-100 );
+
+    // translate(  width/2 , height-100 );
+
+    // CreateTree(Len , Wt );
 
     
-    CreateTree(Len , Wt );
-
-    for()
 }
   
 function draw() {
-    background(220);
+    // background(220);
 }
 
 function CreateTree( l ,w){
-    if( l<20) return ;
+    if( l<10) return ;
 
     strokeWeight(w);
     line( 0 ,0 ,0 ,-l );
@@ -36,4 +37,15 @@ function CreateTree( l ,w){
         pop()
 
     pop()
+}
+
+function TreeArray(){
+    for(let i=100 ;i<width+Shift ; i+=200 ){
+        for(let j=150 ;j<height+Shift ; j+=200 ){
+            push();
+                translate( i , j );
+                CreateTree(Len , Wt);
+            pop();
+        }
+    }
 }
