7093023b4512eb1aa87450a768fd895c48845211
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-06-27 11:11:48 +0800

    setup

---
+++ linked-list.js
@@ -0,0 +1,27 @@
+function setup() {
+    createCanvas( windowWidth , windowHeight );
+}
+  
+function draw() {
+    background(220);
+}
+
+class List {
+    constructor(){
+        
+    }
+
+    Draw(){
+
+    }
+}
+
+class Cube {
+    constructor(){
+
+    }
+
+    Draw(){
+
+    }
+}
