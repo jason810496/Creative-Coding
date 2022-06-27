0cf40c0e986d8ac0ec3c3b90dcb0dd6d03901197
jason810496 <810496@email.wlsh.tyc.edu.tw>
2022-06-27 13:14:28 +0800

    Buggy

--- linked-list.js
+++ linked-list.js
@@ -1,27 +1,74 @@
+var obj;
+
 function setup() {
     createCanvas( windowWidth , windowHeight );
+    
+    obj = new List( 0 , 0 );
 }
-  
+
+
+
 function draw() {
-    background(220);
+    translate( windowWidth/2 , windowHeight/2 );
+    background(0);
+    obj.Update();
 }
 
 class List {
-    constructor(){
-        
+    constructor( x,y ){
+        this.pos = {
+            x:x,
+            y:y,
+        };
+      
+        this.arr = [];
+        this.n = int(random(5,10));
+
+        for(let i=0;i<this.n;i++){
+          this.arr.push( new Cube(i*10,0) );
+        }
     }
 
-    Draw(){
+    Update(){
+      for(let i=this.n-1;i>=1;i--){
+        this.arr[i].pos = this.arr[i-1].pos;
+      }
+      let newX = noise( this.pos.x /100 , this.pos.y/100 , frameCount  );
+      let newY = noise( this.pos.x/100 , this.pos.y/100 , -frameCount );
+      this.arr[0].pos.x = newX;
+      this.arr[0].pos.y = newY;
 
+        push();
+        fill(200);
+      for(let i=0;i<this.n;i++){
+          this.arr[i].Update();
+
+          if( i!=this.n-1 ){
+              push();
+                stroke(200);
+                strokeWeight(10);
+                line( this.arr[i].pos.x , this.arr[i].pos.y , this.arr[i+1].pos.x , this.arr[i+1].pos.y );
+              pop();
+          }
+      }
+
+      pop();
     }
 }
 
 class Cube {
-    constructor(){
+    constructor( x,  y){
+        this.pos = {
+            x:x,
+            y:y,
+        };
+
+        this.w = 50;
+        this.h = 50;
 
     }
 
-    Draw(){
-
+    Update(){
+        rect( this.x-this.w/2 , this.y-this.h/2 , this.x+this.w/2 , this.y+this.h/2 );
     }
 }
